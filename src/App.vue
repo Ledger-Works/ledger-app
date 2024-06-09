e5d66387ced2f1d022e51c4c4467cd8f8f69468a
giteshk11 <writemail2gitesh@gmail.com>
2024-06-09 18:07:37 +0530

    wip: show expenses list

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="app-container">
+  <div class="app-container dark">
     <RouterView />
   </div>
 </template>
--- src/components/expenses/ExpenseListItem.vue
+++ src/components/expenses/ExpenseListItem.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import ShoppingBagIcon from '@/components/icons/ShoppingBagIcon.vue';
+import {
+  Card,
+  CardContent,
+} from '@/components/ui/card'
 </script>
 
 <template>
-  <div class="flex items-center w-full justify-between">
-    <div class="flex gap-x-2">
-      <ShoppingBagIcon class="w-10 h-10" />
-      <div class="">
-        <div class="text-md">
-          Shopping
+  <Card>
+    <CardContent class="flex p-2 justify-between items-center">
+      <div class="flex gap-x-2">
+        <div class="w-10 h-10 p-2 self-center bg-yellow-100 rounded-md">
+          <ShoppingBagIcon class="text-primary-foreground" />
         </div>
-        <div class="text-sm">
-          Buy some grocery
+        <div class="flex flex-col gap-y-1">
+          <span class="text-sm">
+            Shopping
+          </span>
+          <span class="text-xs">
+            Buy some grocery
+          </span>
         </div>
       </div>
-    </div>
-    <div>
-      -$120
-    </div>
-  </div>
+      <div class="text-red-400">
+        -$120
+      </div>
+    </CardContent>
+  </Card>
 </template>
